f3f93c6a697f409eb9113a19fa2e897694a63ee8
FriendlyC0der <1lambm37@solent.ac.uk>
2023-05-18 20:56:04 +0100

    build3

--- board-ai/src/main/java/com/imageBoardAI/boardai/Controllers/PostController.java
+++ board-ai/src/main/java/com/imageBoardAI/boardai/Controllers/PostController.java
@@ -1,6 +1,8 @@
 package com.imageBoardAI.boardai.Controllers;
 
+import com.imageBoardAI.boardai.DAO.PostRepository;
 import com.imageBoardAI.boardai.Entety.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,11 @@ import java.time.LocalDateTime;
 @RequestMapping("/posts")
 public class PostController {
 
+    PostRepository postRepository;
+    @Autowired
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 
     @GetMapping()
     public String getAllPosts(Model model){
